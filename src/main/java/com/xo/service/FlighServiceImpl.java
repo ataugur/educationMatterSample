e9e68c1482fe0499f416f68c5f13da0efccb7787
Hakan S. Kucuk <hakan.kucuk@ignitetech.com>
2018-05-29 00:31:11 +0300

    Update FlighServiceImpl.java

--- src/main/java/com/xo/service/FlighServiceImpl.java
+++ src/main/java/com/xo/service/FlighServiceImpl.java
@@ -18,29 +18,14 @@ class FlighServiceImpl implements FlightService {
     private CityService cityService;
     private FlightRepository flightRepository;
 
-    FlighServiceImpl(CityService cityService, FlightRepository flightRepository) {
-        this.cityService = cityService;
-        this.flightRepository = flightRepository;
-    }
-
     @Override
     public Flight addFlight(FlightDto flightDto) {
-        log.debug("Adding flight: {}", flightDto);
-        if (flightDto.getFromCityCode().equals(flightDto.getToCityCode())) {
-            throw new FlightException("Flight cities can not be same");
-        }
-        City fromCity = cityService.getCity(flightDto.getFromCityCode());
-        City toCity = cityService.getCity(flightDto.getToCityCode());
-        Flight flight = new Flight(fromCity, toCity, flightDto.getFlightNo(), flightDto.getFlightDate());
-        return flightRepository.save(flight);
+        return null;
     }
     
     
      @Override
      public List<Flight> listFlights(String fromCityCode, String toCityCode, Date date) {
-         log.debug("Listing flights from {}, to city {}, date {}", fromCityCode, toCityCode, date);
-         City fromCity = cityService.getCity(fromCityCode);
-         City toCity = cityService.getCity(toCityCode);
-         return flightRepository.findByFromAndToAndDate(fromCity, toCity, date);
+        return null;
      }
 }
