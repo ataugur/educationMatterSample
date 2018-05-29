3c3d35091fb282e6ed4183e571b1b916784ddc14
Hakan S. Kucuk <hakan.kucuk@ignitetech.com>
2018-05-29 16:55:51 +0300

    Update CityServiceImplTest.java

--- src/test/java/com/xo/service/CityServiceImplTest.java
+++ src/test/java/com/xo/service/CityServiceImplTest.java
@@ -15,35 +15,4 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class CityServiceImplTest {
-    private static final String SAMPLE_CITY_CODE = "cityCode";
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-
-    private CityRepository cityRepository = mock(CityRepository.class);
-    private CityServiceImpl cityService = new CityServiceImpl(cityRepository);
-
-    @Test
-    public void whenGetCityThenReturnCity() {
-        //Arrange
-        final City city = new City();
-        when(cityRepository.findByCode(anyString())).thenReturn(city);
-
-        //Act
-        City result = cityService.getCity(SAMPLE_CITY_CODE);
-
-        //Assert
-        verify(cityRepository).findByCode(anyString());
-        assertThat(result, is(city));
-    }
-
-    @Test
-    public void givenWrongCityCodewhenGetCityThenThrowException() {
-        //Arrange
-        when(cityRepository.findByCode(anyString())).thenReturn(null);
-        expectedException.expect(CityNotFoundException.class);
-
-        //Act
-        cityService.getCity(SAMPLE_CITY_CODE);
-    }
 }
