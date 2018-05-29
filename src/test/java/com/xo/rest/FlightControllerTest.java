057b86c0f672f2085f1e37768368a505d8a9cfe1
Hakan S. Kucuk <hakan.kucuk@ignitetech.com>
2018-05-29 16:56:49 +0300

    Update FlightControllerTest.java

--- src/test/java/com/xo/rest/FlightControllerTest.java
+++ src/test/java/com/xo/rest/FlightControllerTest.java
@@ -20,44 +20,5 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class FlightControllerTest {
-    private static final String SAMPLE_CITY_CODE_LA = "LA";
-    private FlightService flightService = mock(FlightService.class);
-    private FlightController flightController = new FlightController(flightService);
-
-    @Test
-    public void whenAddFlightThenReturnAddedFlight() {
-        //Arrange
-        final City city = mock(City.class);
-        final Flight flight = new Flight();
-        flight.setFrom(city);
-        flight.setTo(city);
-        when(flightService.addFlight(any())).thenReturn(flight);
-        FlightDto flightDto = new FlightDto(flight);
-
-        //Act
-        FlightDto result = flightController.addFlight(flightDto);
-
-        //Assert
-        verify(flightService).addFlight(any());
-        assertNotNull(result);
-    }
-
-    @Test
-    public void whenListFlightsThenReturnFlightList() {
-        //Arrange
-        final City city = mock(City.class);
-        Flight flight = new Flight();
-        flight.setFrom(city);
-        flight.setTo(city);
-        final List<Flight> flightList = new ArrayList<>();
-        flightList.add(flight);
-        when(flightService.listFlights(anyString(), anyString(), any(Date.class))).thenReturn(flightList);
-
-        //Act
-        List<FlightDto> result = flightController.listFlights(SAMPLE_CITY_CODE_LA, SAMPLE_CITY_CODE_LA, new Date());
-
-        //Assert
-        verify(flightService).listFlights(anyString(), anyString(), any(Date.class));
-        assertThat(result.size(), is(1));
-    }
+   
 }
